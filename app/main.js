ed14e59685d284e593d9d32213d8a76127e66943
Mahmoud Abo-Zied <116457362+EpitomeZied@users.noreply.github.com>
2026-04-15 10:47:59 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -5,13 +5,13 @@ console.log("Logs from your program will appear here!");
 
 const server = net.createServer((connection) => {
     connection.on("data", (data) => {
-        const commend = data.toString().split("\r\n");
-        if (commend[2] === "ping"){
-            connection.write(`+PONG\r\n`);
-        }else if(commend[2] === "echo"){
-            connection.write(`+${commend[3]}\r\n`);
+        const command = data.toString().split("\r\n");
+        if (command[2] === "PING") {
+            connection.write("+PONG\r\n");
+        } else if (command[2] === "ECHO") {
+            const message = command[4];
+            connection.write(`$${message.length}\r\n${message}\r\n`);
         }
-
     });
 });
 
